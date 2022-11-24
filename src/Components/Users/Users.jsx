c14707d8d821ee80e07ab93f96a6a7de63dff70c
Dmitrii Karpenko <ixdixdi@gmail.com>
2022-11-24 18:16:18 +0100

    52 lesson. Users get from Server Don't work Messages.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,45 +1,21 @@
 import React from "react";
 import styles from './users.module.css';
+import axios from 'axios'
+import userPhoto from '../../assets/images/icons-user-profile.png'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-
-        props.setUsers([
-                {
-                    id: 1,
-                    photoURL: 'https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1956&q=80',
-                    followed: true,
-                    fullName: 'Dmitry',
-                    status: 'Где-то не в себе...',
-                    location: {city: 'Malaga', country: 'Spain'}
-                },
-                {
-                    id: 2,
-                    photoURL: 'https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1956&q=80',
-                    followed: false,
-                    fullName: 'Vasya',
-                    status: 'В себе, но не где-то...',
-                    location: {city: 'Питер', country: 'Россия'}
-                },
-                {
-                    id: 3,
-                    photoURL: 'https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1956&q=80',
-                    followed: true,
-                    fullName: 'Иван Петрович',
-                    status: 'Бездельники опять тут...',
-                    location: {city: 'Морква', country: 'Красная площадь'}
-                }
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        });
     }
-
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoURL} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
@@ -51,12 +27,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
 

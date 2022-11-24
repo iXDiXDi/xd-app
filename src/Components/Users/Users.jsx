e6bc9e14bfe3e4d3be26c73847fe2fbd2c59f56a
Dmitrii Karpenko <ixdixdi@gmail.com>
2022-11-24 20:28:42 +0100

    after 53 lesson. Users.jsx -> to class component Don't work Messages.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,29 +3,35 @@ import styles from './users.module.css';
 import axios from 'axios'
 import userPhoto from '../../assets/images/icons-user-profile.png'
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        });
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => (props.unfollow(u.id))}>Unfollow</button>
-                            : <button onClick={() => (props.follow(u.id))}>Follow</button>}
+                            ? <button onClick={() => (this.props.unfollow(u.id))}>Unfollow</button>
+                            : <button onClick={() => (this.props.follow(u.id))}>Follow</button>}
                     </div>
 
 
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -36,9 +42,11 @@ let Users = (props) => {
                     </span>
                 </span>
 
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
+
 }
 
 export default Users;
